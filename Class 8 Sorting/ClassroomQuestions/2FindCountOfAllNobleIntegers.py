00184f98c85cbc999a55bc6cf2a745aabd6e0023
Shubham Chaudhary <shubhamch1504@gmail.com>
2025-01-17 13:50:05 +0530

    a

--- Class 8 Sorting/ClassroomQuestions/2FindCountOfAllNobleIntegers.py
+++ Class 8 Sorting/ClassroomQuestions/2FindCountOfAllNobleIntegers.py
@@ -1,22 +1,37 @@
 # you are given an integer A find all the number of noble integers in the array
 # Noble integer is the integer  # count of elements < element == element itself
 
+# Brute force Approach
+# def solve(A):
+
+#     N = len(A)
+#     returnAnswer = 0
+
+#     for i in range(N):
+#         count = 0
+#         if A[i] >= 0:
+            
+#             for j in range(N):
+
+#                 if A[j] < A[i]:
+#                     count +=1
+
+#         if count == A[i]:
+#             returnAnswer +=1
+
+#     return returnAnswer
+
+
+# Using sorting technigue
 
 def solve(A):
 
     N = len(A)
+    A.sort()
     returnAnswer = 0
 
     for i in range(N):
-        count = 0
-        if A[i] >= 0:
-            
-            for j in range(N):
-
-                if A[j] < A[i]:
-                    count +=1
-
-        if count == A[i]:
+        if A[i] == i:
             returnAnswer +=1
 
     return returnAnswer
